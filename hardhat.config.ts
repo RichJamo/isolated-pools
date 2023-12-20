049e2f6fa4683c792c3cc11730e466965070925a
Corey Rice <coreyarice@gmail.com>
2023-12-20 15:15:17 -0300

    feat: reduce reserves with available cash when accruing interest

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -307,6 +307,9 @@ const config: HardhatUserConfig = {
       {
         artifacts: "node_modules/@venusprotocol/venus-protocol/artifacts",
       },
+      {
+        artifacts: "node_modules/@venusprotocol/protocol-reserve/artifacts",
+      },
     ],
   },
 };
