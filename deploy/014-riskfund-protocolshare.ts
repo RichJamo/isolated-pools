8f3ea75b92a78fcf8788963b5f11d1427d78063f
Richard Jamieson <jamieson.rich@gmail.com>
2024-02-27 23:28:01 +0200

    Debugging riskfund

--- deploy/014-riskfund-protocolshare.ts
+++ deploy/014-riskfund-protocolshare.ts
@@ -13,12 +13,13 @@ const MIN_POOL_BAD_DEBT = convertToUnit(1000, 18);
 const maxLoopsLimit = 100;
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  console.log('got here 0')
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   const { tokensConfig, preconfiguredAddresses } = await getConfig(hre.network.name);
   const usdt = await getUnderlyingToken("USDT", tokensConfig);
-
+  console.log('got here 1')  
   const poolRegistry = await ethers.getContract("PoolRegistry");
   const deployerSigner = ethers.provider.getSigner(deployer);
   const swapRouterAddress = await toAddress(preconfiguredAddresses.SwapRouter_CorePool || "SwapRouter", hre);
@@ -30,11 +31,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const owner = await proxyAdmin.owner();
 
   let corePoolComptrollerAddress = preconfiguredAddresses.Unitroller;
+  console.log('got here 2')
+
   if (!hre.network.live) {
     corePoolComptrollerAddress = (await smock.fake<Comptroller>("Comptroller")).address;
   }
+  console.log('got here 3')
 
-  await deploy("RiskFund", {
+  console.log(await deploy("RiskFund", {
     from: deployer,
     contract: "RiskFund",
     proxy: {
@@ -53,10 +57,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       preconfiguredAddresses.VBNB_CorePool || "0x0000000000000000000000000000000000000001",
       preconfiguredAddresses.WBNB || "0x0000000000000000000000000000000000000002",
     ],
-  });
+  }));
+  console.log('got here 4')
 
   const riskFund = await ethers.getContract("RiskFund");
-
+  console.log(riskFund.address)
   const shortfallDeployment = await deploy("Shortfall", {
     from: deployer,
     contract: "Shortfall",
@@ -72,6 +77,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     autoMine: true,
     log: true,
   });
+  console.log('got here 5')
 
   const shortfall = await ethers.getContract("Shortfall");
   if ((await shortfall.poolRegistry()) !== poolRegistry.address) {
@@ -79,21 +85,28 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const tx = await shortfall.connect(deployerSigner).updatePoolRegistry(poolRegistry.address);
     await tx.wait();
   }
+  console.log('got here 5a')
 
   if ((await riskFund.shortfall()) !== shortfall.address) {
     console.log("Setting Shortfall contract address in RiskFund");
     const tx = await riskFund.setShortfallContractAddress(shortfallDeployment.address);
     await tx.wait(1);
   }
+  console.log('got here 5b')
 
   const contract = await ethers.getContract("RiskFund");
-  if ((await contract.poolRegistry()) !== poolRegistry.address) {
+  console.log('got here 5c')
+  console.log(contract.address)
+  console.log(poolRegistry.address)
+
+  if ((await riskFund.poolRegistry()) !== poolRegistry.address) {
     console.log(`Setting PoolRegistry address in RiskFund contract`);
     const tx = await contract.setPoolRegistry(poolRegistry.address);
     await tx.wait();
   }
-
+  console.log('got here 5d')
   const targetOwner = preconfiguredAddresses.NormalTimelock || deployer;
+  console.log('got here 6')
   for (const contractName of ["RiskFund", "Shortfall"]) {
     const contract = await ethers.getContract(contractName);
     if ((await contract.owner()) !== targetOwner && (await contract.pendingOwner()) !== targetOwner) {
@@ -102,6 +115,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       await tx.wait();
     }
   }
+  console.log('got to the end')
 };
 func.tags = ["RiskFund", "il"];
 
